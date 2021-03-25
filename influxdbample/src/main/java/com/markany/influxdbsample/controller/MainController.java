e9b29f3406d7e06b6b60f3be7bf35756039a5b73
jiyoung <jiyoung4061@naver.com>
2021-03-25 15:46:28 +0900

    influxdb

--- influxdbample/src/main/java/com/markany/influxdbsample/controller/MainController.java
+++ influxdbample/src/main/java/com/markany/influxdbsample/controller/MainController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,16 +13,21 @@ import com.markany.influxdbsample.dto.H2oFeet;
 import com.markany.influxdbsample.service.MainService;
 
 @Controller
-@RequestMapping(path="/api/v1/NOAAWater")
+@RequestMapping(path="/")
 public class MainController {
 
 	@Autowired
 	private MainService mainService;
 
-
-	@GetMapping(path="/h2os")
-	public @ResponseBody List<H2oFeet> h2o_list() {
-
-		return mainService.h2o_list();
+//	@GetMapping(path="/h2os")
+//	public @ResponseBody List<H2oFeet> h2o_list() {
+//
+//		return mainService.h2o_list();
+//	}
+	
+	@RequestMapping("/main")
+	public List<H2oFeet> h2o_list(@PathVariable("list")String list){
+		 List<H2oFeet> rs=mainService.h2o_list();
+		 return rs;
 	}
 }
