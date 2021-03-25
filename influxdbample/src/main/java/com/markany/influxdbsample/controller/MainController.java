454ba28285a27f7edda8d9ec2f820305d426b326
jiyoung <jiyoung4061@naver.com>
2021-03-25 14:40:09 +0900

    influxdb sample

---
+++ influxdbample/src/main/java/com/markany/influxdbsample/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.markany.influxdbsample.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.markany.influxdbsample.dto.H2oFeet;
+import com.markany.influxdbsample.service.MainService;
+
+@Controller
+@RequestMapping(path="/api/v1/NOAAWater")
+public class MainController {
+
+	@Autowired
+	private MainService mainService;
+
+
+	@GetMapping(path="/h2os")
+	public @ResponseBody List<H2oFeet> h2o_list() {
+
+		return mainService.h2o_list();
+	}
+}
